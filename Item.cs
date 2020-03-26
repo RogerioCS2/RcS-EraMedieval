bb9ee2796e54ab4f2b197b1037ce081a03bd79b2
RogerioCS <tecnologia.rogerio@gmail.com>
2020-03-25 23:04:47 -0300

    Criado Objeto Item

--- Item.cs
+++ Item.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
+    [Header ("Tipos de Itens")]
     public bool isItem;
     public bool isWeapon;
     public bool isArmour;
-
+    [Header ("Caracteristicas Itens")]
     public string itemName;
     public string description;
     public int value;
     public Sprite itemSprite;
-
+    [Header ("Beneficios Itens")]
     public int amountTochange;
     public bool affectHP, affectMP, affectStr;
-
+    [Header ("Detalhes Armas/Armaduras")]
     public int weaponStrength;
     public int armorStrength;
        
