17a44e621cf8b25ce538eb9c0e109c6bf37c85f3
RogerioCS <tecnologia.rogerio@gmail.com>
2020-03-28 02:59:06 -0300

    Criado Coleta de Itens e menu Inicial

---
+++ ColetarItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColetarItem : MonoBehaviour
+{
+    private bool pegarItem;
+
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (pegarItem && Input.GetButtonDown("Fire1") && ControlePersonagem.instance.podeAndar){
+            GameManager.instance.AdicionaItemInventario(GetComponent<Item>().itemName);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D calisao){
+        if (calisao.tag == "Player"){
+            pegarItem = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D calisao){
+        if (calisao.tag == "Player"){
+            pegarItem = false;
+        }
+    }
+}
